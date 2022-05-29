98b8537ce37ca441a8b515233f2a0f95a006e478
tsai <tsai>
2022-05-29 21:57:43 +0800

    clear all

--- GUI_Test/src/test.java
+++
@@ -1,6 +0,0 @@
-public class test {
-	public static void main(String []args) {
-		GUI gui = new GUI();
-		gui.run();
-	}
-}
--- tester/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.tester {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.tester to javafx.fxml;
-    exports com.example.tester;
-}
